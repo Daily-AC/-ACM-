12cdb5f60bae2167516ad2cf300e54d0a06032f6
rongyan-123 <2637754948@qq.com>
2025-04-28 20:04:14 +0800

    笑死我了这题
    蚌埠住了

---
+++ 24计科1班黄家新k/题目+答案+复盘/2056c/1200-2041E.cpp
@@ -0,0 +1,15 @@
+//我真绷不住了，这么点，1200？而且还很难想
+// 但是想出来就真的笑死我了 
+//
+//题目意思是，给一个平均数和中位数，找一个符合条件的数组出来就行 
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+signed main()
+{
+	cin>>a>>b;
+	
+	cout<<"3"<<endl;
+	cout<<3*a-2*b<<" "<<b<<" "<<b;
+	//tm的这样一算真的耍赖 
+}
